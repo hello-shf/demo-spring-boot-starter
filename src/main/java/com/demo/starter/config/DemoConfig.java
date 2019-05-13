652156f95cc4eff0b30bfb09cc350c699e85c84a
sunhuaifeng <877635632@qq.com>
2019-05-13 17:11:54 +0800

    分支提交

--- src/main/java/com/demo/starter/config/DemoConfig.java
+++ src/main/java/com/demo/starter/config/DemoConfig.java
@@ -1,6 +1,5 @@
 package com.demo.starter.config;
 
-import com.demo.starter.properties.Demo1Properties;
 import com.demo.starter.properties.DemoProperties;
 import com.demo.starter.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version V1.0
  **/
 @Configuration
-@EnableConfigurationProperties({Demo1Properties.class, DemoProperties.class})
+@EnableConfigurationProperties(DemoProperties.class)
 @ConditionalOnProperty(
         prefix = "demo",
         name = "isopen",
@@ -25,16 +24,10 @@ import org.springframework.context.annotation.Configuration;
 )
 public class DemoConfig {
     @Autowired
-    private Demo1Properties demo1Properties;
-    @Autowired
     private DemoProperties demoProperties;
 
     @Bean(name = "demo")
     public DemoService demoService(){
         return new DemoService(demoProperties.getSayWhat(), demoProperties.getToWho());
-    }
-    @Bean(name = "demo1")
-    public DemoService  demo1Service(){
-        return new DemoService(demo1Properties.getSayWhat(), demo1Properties.getToWho());
     }
 }
